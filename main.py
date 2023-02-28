dcf1e2d14b0c43197e255ccbca68a35663c1c078
sunil mondal <mondalsunil415@gmail.com>
2023-02-28 22:18:05 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2
+# Load an color image in grayscale
+img = cv2.imread('C:\\Users\\monda\\Downloads\\image.jpeg', 0)
+#  Display the image
+cv2.imshow('image', img)
+#  key binding function
+cv2.waitKey(0)
+#  Destroyed all window we created earlier.
+cv2.destroyAllWindows()
